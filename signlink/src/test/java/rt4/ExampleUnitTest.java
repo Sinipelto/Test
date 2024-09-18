336c60fdbaf8c6563d8ee04a369f334c39e55d97
Sinipelto <sinipelto@peltonet.com>
2024-09-18 15:11:49 +0300

    add example unit test. configure gradle. update gradle to more compatible version.

---
+++ signlink/src/test/java/rt4/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package rt4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ExampleUnitTest {
+		@Test
+		void exampleTest() {
+			assertEquals(3, 1+1);
+		}
+}
